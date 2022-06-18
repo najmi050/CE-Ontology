00da39d3f05d1396c62aeaa5bc88e8e3d04c5e15
najmi050 <92123280+najmi050@users.noreply.github.com>
2022-06-18 22:25:35 +0100

    Update webvowl.js

--- doc/webvowl/js/webvowl.js
+++ doc/webvowl/js/webvowl.js
@@ -1,5 +1,5 @@
 webvowl =
-/******/ (function(modules) { // webpackBootstrap
+/******/ (function(modules) { // webpackBootstrap 
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
 
